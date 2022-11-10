5ac2d87f2584f9b8725e219371a8d617169d0856
pwtail <abvit89s@gmail.com>
2022-11-10 05:24:39 +0100

    ipy: finally

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="greenhack",
-      version="0.1.11",
+      version="0.1.12",
       author="Vitalii Abetkin",
       author_email="abvit89s@gmail.ru",
       packages=find_packages(),
