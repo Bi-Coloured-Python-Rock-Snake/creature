80e7564f9315b4aac0092a57510c047e3cfa5ace
pwtail <abvit89s@gmail.com>
2022-11-10 05:23:32 +0100

    ipy: finally

--- greenhack/ipy.py
+++ greenhack/ipy.py
@@ -1,30 +1,23 @@
-import asyncio
-from concurrent.futures import ThreadPoolExecutor
+import greenlet
+from IPython.core.async_helpers import _pseudo_sync_runner as sync_runner, _asyncio_runner
 
-from IPython.core.async_helpers import _pseudo_sync_runner as sync_runner
-from IPython.terminal.interactiveshell import TerminalInteractiveShell
-
-from greenhack import start_loop
+from greenhack._loop import _loop
 
 
-def thread_pool(max_workers):
-    return ThreadPoolExecutor(max_workers=max_workers, initializer=init_thread)
-
-
-def init_thread():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    start_loop()
-
-
-def loop_runner(co, pool=thread_pool(1)):
-    return pool.submit(sync_runner, co).result()
+def loop_runner(co):
+    sync_greenlet = greenlet.greenlet(sync_runner)
+    sync_greenlet.async_greenlet = (current := greenlet.getcurrent())
+    current.sync_greenlet = sync_greenlet
+    task = sync_greenlet.switch(co)
+    return _asyncio_runner(_loop(task))
 
 
 def enable():
+    from IPython.terminal.interactiveshell import TerminalInteractiveShell
     # Sorry, trio runner!
     TerminalInteractiveShell.instance().trio_runner = loop_runner
 
 
 def disable():
+    from IPython.terminal.interactiveshell import TerminalInteractiveShell
     TerminalInteractiveShell.instance().trio_runner = None
